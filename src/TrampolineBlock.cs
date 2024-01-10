0b18ee9c2ae443b33588e57d0bdaf785f4c9f05c
CozyLilKitty <156186422+CozyLilKitty@users.noreply.github.com>
2024-01-10 16:01:42 +0200

    Add files via upload

---
+++ src/TrampolineBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace Trampoline.src
+{
+    public class TrampolineBlock : Block
+    {
+        public AssetLocation tickSound = new AssetLocation("game", "tick");
+
+        public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact)
+        {
+            if (isImpact && facing.Axis == EnumAxis.Y)
+            {
+                world.PlaySoundAt(tickSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
+                entity.Pos.Motion.Y *= -0.8;
+            }
+        }
+    }
+}
---
+++ src/TrampolineModSystem.cs
@@ -0,0 +1,18 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace Trampoline.src
+{
+    public class TrampolineModSystem : ModSystem
+    {
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterBlockClass("trampoline", typeof(TrampolineBlock));
+        }
+    }
+}
